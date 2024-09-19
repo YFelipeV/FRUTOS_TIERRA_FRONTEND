ef79f1a52ed1c13f8c16861863dba007e33b6509
YFelipeV <felipesegura250@gmail.com>
2024-09-18 19:26:37 -0500

    se modifica archivo config

--- src/config/enviroment.js
+++ src/config/enviroment.js
@@ -1,4 +1,5 @@
-export const API1 = 'https://frutos-tierra.onrender.com/api/';
-export const API = 'http://localhost:4000/api/';
-export const API_FRONTEND = 'http://127.0.0.1:5173/';
-export const PHONE = '3127929504';
+export const API = 'https://frutos-tierra-api.onrender.com/api/';
+export const API2 = 'http://localhost:4000/api/';
+export const API_FRONTEND = 'https://frutos-tierra-frontend.onrender.com/';
+export const API_FRONTEND2 = 'http://127.0.0.1:5173/';
+export const PHONE = '3112474569';
