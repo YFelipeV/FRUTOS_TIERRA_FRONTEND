634a0fb666a72214e4b71e47024a1c36c6037ccf
Felipe Segura <116193896+YFelipeV@users.noreply.github.com>
2024-09-22 18:45:04 -0500

    Update enviroment.js

--- src/config/enviroment.js
+++ src/config/enviroment.js
@@ -1,5 +1,4 @@
-//export const API = 'https://frutos-tierra-api.onrender.com/api/';
-export const API = 'http://localhost:4000/api/';
+export const API = 'https://frutos-tierra-api.onrender.com/api/';
+export const API_DEVELOP = 'http://localhost:4000/api/';
 export const API_FRONTEND = 'https://frutos-tierra-frontend.onrender.com/';
-export const API_FRONTEND2 = 'http://127.0.0.1:5173/';
 export const PHONE = '3112474569';
